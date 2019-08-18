95852c8cc03cade65211d7e998379ba659c36130
Igor Leme <igor.leme.m@outlook.com>
2019-08-18 01:38:54 -0300

    Endpoint /pedidos/{id} disponibilizado

---
+++ src/main/java/com/igor/cursomc/service/PedidoService.java
@@ -0,0 +1,23 @@
+package com.igor.cursomc.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.igor.cursomc.domain.Pedido;
+import com.igor.cursomc.repositories.PedidoRepository;
+import com.igor.cursomc.service.exceptions.ObjectNotFoundException;
+
+@Service
+public class PedidoService {
+	
+	@Autowired
+	private PedidoRepository repo;
+	
+	public Pedido buscar(Integer id) {
+		Optional<Pedido> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado! Id: " + id + ", Tipo: " 
+				   + Pedido.class.getName()));
+	}
+}
