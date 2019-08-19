704f7f6dddc8376202d0f58695b66c14e038148e
Igor Leme <igor.leme.m@outlook.com>
2019-08-19 17:33:00 -0300

    Validacao com Bean Validation na classe Categoria

--- src/main/java/com/igor/cursomc/service/CategoriaService.java
+++ src/main/java/com/igor/cursomc/service/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.igor.cursomc.domain.Categoria;
+import com.igor.cursomc.dto.CategoriaDTO;
 import com.igor.cursomc.repositories.CategoriaRepository;
 import com.igor.cursomc.service.exceptions.DataIntegrityException;
 import com.igor.cursomc.service.exceptions.ObjectNotFoundException;
@@ -54,5 +55,9 @@ public class CategoriaService {
 	public Page<Categoria> findPage(Integer page, Integer linesPerPage, String orderBy, String direction){
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDTO) {
+		return new Categoria(objDTO.getId(), objDTO.getNome());
 	}
 }
