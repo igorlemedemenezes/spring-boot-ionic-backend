a32faf0d6cb3ec0706ae275da80ff0ad51c88064
Igor Leme <igor.leme.m@outlook.com>
2019-09-02 13:29:02 -0300

    Validacao customizada, email não repitido na atualizacao de cliente

--- src/main/java/com/igor/cursomc/dto/ClienteDTO.java
+++ src/main/java/com/igor/cursomc/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import com.igor.cursomc.domain.Cliente;
+import com.igor.cursomc.service.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable{
 	private static final long serialVersionUID = 1L;
 	
