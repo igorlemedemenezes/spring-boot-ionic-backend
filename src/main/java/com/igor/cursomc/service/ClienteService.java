3e5bc73a6bda5c4188fc30e0a54113c81e1a85a6
Igor Leme <grupoevu@gmail.com>
2019-11-16 16:50:52 -0300

    Endpoint para fotos de usuarios cmo Multipart File

--- src/main/java/com/igor/cursomc/service/ClienteService.java
+++ src/main/java/com/igor/cursomc/service/ClienteService.java
@@ -1,5 +1,6 @@
 package com.igor.cursomc.service;
 
+import java.net.URI;
 import java.util.List;
 import java.util.Optional;
 
@@ -12,6 +13,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import com.igor.cursomc.domain.Cidade;
 import com.igor.cursomc.domain.Cliente;
@@ -39,9 +41,8 @@ public class ClienteService {
 	@Autowired
 	private BCryptPasswordEncoder pe;
 	
-
-	
-		
+	@Autowired
+	private S3Service s3Service;
 	
 	public Cliente find(Integer id) {
 		UserSS user = UserService.authenticated();
@@ -108,4 +109,9 @@ public class ClienteService {
 		newObj.setNome(obj.getNome());
 		newObj.setEmail(obj.getEmail());
 	}
+	
+	public URI uploadProfilePicture(MultipartFile multipartFile) {
+		return s3Service.uploadFile(multipartFile);
+	}
+	
 }
