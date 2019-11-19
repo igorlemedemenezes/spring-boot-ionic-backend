173363f147c8cad770caf6d2eb2a8d47f50eafc1
Igor Leme <grupoevu@gmail.com>
2019-11-18 21:44:13 -0300

    Linkando jsp com controller

--- src/main/java/com/igor/cursomc/CursomcApplication.java
+++ src/main/java/com/igor/cursomc/CursomcApplication.java
@@ -1,17 +1,23 @@
 package com.igor.cursomc;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CursomcApplication implements CommandLineRunner{
+public class CursomcApplication extends SpringBootServletInitializer /* implements CommandLineRunner */ {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(CursomcApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//	}
 }
